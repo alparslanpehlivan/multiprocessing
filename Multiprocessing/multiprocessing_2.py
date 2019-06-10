2a28a5099c32dc1068f9d0d61d9ce8f4bf0df914
Alparslan <50515844+alparslanpehlivan@users.noreply.github.com>
2019-06-10 19:50:01 +0300

    -

---
+++ Multiprocessing/multiprocessing_2.py
@@ -0,0 +1,39 @@
+
+import os
+import time
+
+from multiprocessing import Process, current_process
+
+
+def square(numbers):
+    for number in numbers:
+        time.sleep(10)
+        result = number * number
+        print(f"The number {number} squares to {result}.")
+
+    #process_id = os.getpid()
+    #print(f"Process ID: {process_id}")
+
+    #process_name= current_process().name
+    #print(f"Process Name: {process_name}")
+
+    #print("The number "+str(number) + " squares to "+ str(result))
+
+if __name__ == "__main__":
+
+    #processes = []
+    processes = []
+    number = range(100)
+
+    for i in range(50):
+        process = Process(target=square, args=(number,))
+        processes.append(process)
+
+        # Processes are spawned by creating a Process object and
+        # then calling its start() method
+        process.start()
+
+    for process in processes:
+        process.join()
+
+    print("Multiprocessing complete")
